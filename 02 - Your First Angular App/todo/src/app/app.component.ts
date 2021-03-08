b2120fc68e65f8ffe33f8f63ad022921bb0745ad
Tomek Plewniak <tomekplewniak@gmail.com>
2021-03-08 16:35:27 +0100

    Prepare template and component.

--- 02 - Your First Angular App/todo/src/app/app.component.ts
+++ 02 - Your First Angular App/todo/src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { Model } from './model';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector:"todo-app",
+  templateUrl: "app.component.html"
 })
-export class AppComponent {
-  title = 'todo';
+export class AppComponent{
+  model = new Model();
+
+  getName(){
+    return this.model.user;
+  }
 }
