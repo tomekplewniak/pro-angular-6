519d3121b813b812968c2e464fc3ce46749f5620
Tomek Plewniak <37481429+tomekplewniak@users.noreply.github.com>
2021-03-14 18:51:47 +0100

    Feature/chapter 2 (#1)
    * Create a new project.
    
    * Add bootstrap.
    
    * Update index file.
    
    * Prepare template and component.
    
    * Add FormsModule.
    
    * Add Todo table.
    
    * Create a Two-Way Data Binding.
    
    Use in this purpose checkbox.
    
    * Filter To-Do Items.
    
    * Add todo item.
    
    * Fix table.

---
+++ 02 - Your First Angular App/todo/src/app/model.ts
@@ -0,0 +1,21 @@
+export class Model {
+    user: string;
+    items: TodoItem[];
+
+    constructor() {
+        this.user = "Thomas";
+        this.items = [new TodoItem("Buy Flowers", false),
+        new TodoItem("Get Shoes", false),
+        new TodoItem("Collect Tickets", false),
+        new TodoItem("Call Joe", false)]
+    }
+}
+
+export class TodoItem {
+    action: string;
+    done: boolean;
+    constructor(action: string, done: boolean) {
+        this.action = action;
+        this.done = done;
+    }
+}
